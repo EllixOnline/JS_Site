912cd4b4e3d41460b9542d26ce099078ea6c62a2
EllixOnline <3likinlisa@gmail.com>
2024-07-25 22:23:52 +0300

    XXX

--- code.js
+++ code.js
@@ -2,6 +2,68 @@
 
 
 
+const accounts = 
+[
+    {
+     name: "Nikita",
+     lastname: "Katsovich",
+     phone: "0546850131",
+     email: "ellixonline@gmail.com",
+     city: "Haifa",
+     address: "Yad La Banim 206" ,
+     profilepiclink: "./images/ProfilePicPlaceholder.jpg"  
+    },
+
+    {
+        name: "Amit",
+        lastname: "Nosayev",
+        phone: "0544883203",
+        email: "abednosayev@gmail.com",
+        city: "Or Akiva",
+        address: "CityStreet 20" ,
+        profilepiclink: "./images/ProfilePicPlaceholder.jpg" 
+    },
+
+    {
+        name: "Rachel",
+        lastname: "Silver",
+        phone: "0546669991",
+        email: "mishkahandesaim@gmail.com",
+        city: "Ofakim",
+        address: "Uriel 55" ,
+        profilepiclink: "./images/ProfilePicPlaceholder.jpg" 
+    }
+
+]
+
+create();
+
+function create() {
+    let content = "";
+    const container = document.getElementsByClassName("contact_container")[0];
+    accounts.forEach((elem, index) => {
+        content += `
+        <div class="contact_div">
+            <div class="contact_image">
+                <img src="" alt="Error" onerror="this.onerror=null; this.src='./images/ProfilePicPlaceholder.jpg'">
+            </div>
+            <div class="contact_info">
+                <div>
+                    <b><u>Name</u>:</b> ${elem.name} <br>
+                    <b><u>Phone</u>:</b> ${elem.phone} <br>
+                </div>
+            </div>
+            <div class="contact_buttons">
+                <img class="hover-effect" src="./images/Icons/IcoInfo.png" onclick="InfoContact(${index})">
+                <img class="hover-effect" src="./images/Icons/IcoEdit.png" onclick="EditContact(${index})">
+                <img class="hover-effect" src="./images/Icons/IcoRemove.png" onclick="RemoveContact(${index})">
+            </div>
+        </div>`;
+    });
+
+    container.innerHTML += content;
+}
+
 function myFunction() 
 {
     let overlayelement = document.getElementById('screenoverlay');
@@ -25,3 +87,45 @@ function myFunction()
        
     }
   }
+
+  function DeleteAll()
+  {
+    if (confirm(`Do you want to DELETE ALL CONTACTS?`))
+    {
+    const container = document.getElementsByClassName("contact_container")[0];
+    container.innerHTML = "";
+    accounts = [{}];
+    create();
+    }
+  }
+
+  function InfoContact(index) {
+    let overlayElement = document.getElementById('screenoverlay');
+    overlayElement.classList.add('full-screen-overlay');
+    overlayElement.classList.remove('hidden_class');
+    let popupContent = document.createElement('div');
+    popupContent.classList.add('popup_window');
+    popupContent.innerHTML = `
+        <form>
+            <label>Name: ${accounts[index].name}</label><br>
+            <label>Last Name: ${accounts[index].lastname}</label><br>
+            <label>Phone: ${accounts[index].phone}</label><br>
+            <label>Email: ${accounts[index].email}</label><br>
+            <label>Address: ${accounts[index].city}<br>${accounts[index].address}</label>
+        </form>
+    `;
+
+    overlayElement.appendChild(popupContent);
+}
+
+function RemoveContact (index)
+
+{
+    if (confirm(`Do you want to DELETE ${accounts[index].name}'s accout? `))
+    {
+    const container = document.getElementsByClassName("contact_container")[0];
+    container.innerHTML = "";
+    accounts.splice(index, 1);
+    create();
+    }
+}
