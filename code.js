6e6b808635874c98715fe73a6b7c69a909d07397
EllixOnline <3likinlisa@gmail.com>
2024-08-21 20:24:50 +0300

    Latest

--- code.js
+++ code.js
@@ -1,5 +1,25 @@
 'use strict'
 
+function preloadImages() {
+    var images = ['Palm_Tree_BG_Main.png',
+         'Palm_Tree_BG_Party1.png',
+          'Palm_Tree_BG_Party2.png',
+           'Palm_Tree_BG_Party3.png',
+            'Palm_Tree_BG_Party4.png',
+             'Palm_Tree_BG_Party5.png'];
+    for (var i = 0; i < images.length; i++) {
+        var img = new Image();
+        img.src = images[i];
+    }
+}
+
+document.getElementById("screenoverlay").addEventListener("click", function(e) {
+
+    if (e.target.id === "screenoverlay") {
+        InfoContactClose();
+    }
+});
+
 const accounts = 
 [
     {
@@ -9,7 +29,7 @@ const accounts =
      email: "ellixonline@gmail.com",
      city: "Haifa",
      address: "Yad La Banim 206" ,
-     profilepiclink: "./images/ProfilePicPlaceholder.jpg"  
+     profilepiclink: "https://scontent.ftlv27-1.fna.fbcdn.net/v/t39.30808-6/454715446_10219141748894045_7533506083240458874_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=dBG77gs9i4sQ7kNvgEJxnjU&_nc_ht=scontent.ftlv27-1.fna&oh=00_AYCIzsjw3zqGgJo5saZnrSdrQTd7cKAR_591b69DuBACCA&oe=66CBDABA"  
     },
 
     {
@@ -19,7 +39,7 @@ const accounts =
         email: "abednosayev@gmail.com",
         city: "Or Akiva",
         address: "CityStreet 20" ,
-        profilepiclink: "./images/ProfilePicPlaceholder.jpg" 
+        profilepiclink: "https://i.gyazo.com/36b37c34a47b2d7f02ca802132fed12c.jpg" 
     },
 
     {
@@ -56,11 +76,11 @@ function create(search) {
             content += `
             <div class="contact_div">
                 <div class="contact_image">
-                    <img src="" alt="Error" class="hover-effect" onerror="this.onerror=null; this.src='./images/ProfilePicPlaceholder.jpg'">
+                    <img <img src="${elem.profilepiclink}" alt="Description" class="hover-effect" onerror="this.onerror=null; this.src='./images/ProfilePicPlaceholder.jpg'">
                 </div>
                 <div class="contact_info">
                     <div>
-                        <b><u>Name</u>:</b> ${elem.name} <br>
+                        <b><u>Name</u>:</b> ${elem.name} ${" "} ${elem.lastname} <br>
                         <b><u>Phone</u>:</b> ${elem.phone} <br>
                     </div>
                 </div>
@@ -77,16 +97,16 @@ function create(search) {
     {
     accounts.forEach((elem, index) => 
         {
-        if(elem.name.includes(search) || elem.phone.includes(search))
+        if(elem.name.includes(search) || elem.phone.includes(search) || elem.lastname.includes(search))
         {
         content += `
         <div class="contact_div">
             <div class="contact_image">
-                <img src="" alt="Error" onerror="this.onerror=null; this.src='./images/ProfilePicPlaceholder.jpg'">
+                <img <img src="${elem.profilepiclink}" alt="Description" class="hover-effect" onerror="this.onerror=null; this.src='./images/ProfilePicPlaceholder.jpg'">
             </div>
             <div class="contact_info">
                 <div>
-                    <b><u>Name</u>:</b> ${elem.name} <br>
+                    <b><u>Name</u>:</b> ${elem.name} ${" "} ${elem.lastname} <br>
                     <b><u>Phone</u>:</b> ${elem.phone} <br>
                 </div>
             </div>
@@ -112,31 +132,6 @@ function searchIt()
 }
 
 
-
-function myFunction() 
-{
-    let overlayelement = document.getElementById('screenoverlay');
-
-    alert = document.getElementById('screenoverlay');
-
-    if (overlayelement && !overlayelement.classList.contains('full-screen-overlay')) 
-    {
-        console.log("changed from hidden to visible");
-        overlayelement.classList.add('full-screen-overlay');
-        overlayelement.classList.remove('hidden_class');
-
-    } 
-    
-    else 
-    {
-        console.log("changed from visible to hidden");
-        overlayelement.classList.add('hidden_class');
-        overlayelement.classList.add('popup_window');
-        overlayelement.classList.remove('full-screen-overlay');
-       
-    }
-  }
-
   function DeleteAll()
   {
     if (confirm(`Do you want to DELETE ALL CONTACTS?`))
@@ -161,12 +156,15 @@ function myFunction()
         </div>
         <div class='information'>
         <form>
-            <label>Name: ${accounts[index].name}</label><br>
-            <label>Last Name: ${accounts[index].lastname}</label><br>
-            <label>Phone: ${accounts[index].phone}</label><br>
-            <label>Email: ${accounts[index].email}</label><br>
-            <label>Address: ${accounts[index].city}<br>${accounts[index].address}</label>
+            <label><b>Name:</b> ${accounts[index].name}</label><br>
+            <label><b>Last Name:</b> ${accounts[index].lastname}</label><br>
+            <label><b>Phone:</b> ${accounts[index].phone}</label><br>
+            <label><b>Email:</b> ${accounts[index].email}</label><br>
+            <label><b>Address:</b> ${accounts[index].city}<br>${accounts[index].address}</label>
         </form>
+        <div class="pic">
+        <img src='${accounts[index].profilepiclink}'>
+        <div>
         </div>
         </div>
     `;
@@ -177,6 +175,7 @@ function myFunction()
 function InfoContactClose()
 {
     let overlayElement = document.getElementById('screenoverlay');
+    overlayElement.innerHTML = '';
     overlayElement.classList.add('hidden_class');
     overlayElement.classList.remove('full-screen-overlay');
 }
@@ -197,21 +196,63 @@ function AddContact() {
         <div class='information'>
         <form>
             <label>Name:</label><br>
-            <input type="text" id="name" placeholder="name"></input><br>
+            <input type="text" id="name" placeholder="First name"></input><br>
             <label>Last Name:</label><br>
-            <input type="text" id="lastname" placeholder="last name"></input><br>
+            <input type="text" id="lastname" placeholder="Last name"></input><br>
             <label>Phone:</label><br>
-            <input type="number" id="phone" placeholder="phone"></input><br>
+            <input type="number" id="phone" placeholder="Phone number"></input><br>
             <label>Email:</label><br>
-            <input type="text" id="email" placeholder="email"></input><br>
+            <input type="text" id="email" placeholder="E-mail"></input><br>
             <label>City:</label><br>
-            <input type="text" id="city" placeholder="city"></input><br>
+            <input type="text" id="city" placeholder="City"></input><br>
             <label>Address:</label><br>
-            <input type="text" id="address" placeholder="address"></input><br>
+            <input type="text" id="address" placeholder="Address in the city"></input><br>
             <label>Image URL:<br>
-            <input type="text" id="image" placeholder="address"></input><br>
+            <input type="text" id="image" placeholder="Image to use as avatar's URL"></input><br>
         </form>
-        <button class="buttonAdd" onclick="sendInfo()">Add</button>
+        <br>
+        <button class="sendinfobutton" onclick="sendInfo()">Add</button>
+        </div>
+        </div>
+    `;
+
+    overlayElement.appendChild(popupContent);
+
+    
+
+}
+
+function EditContact(index) {
+    let overlayElement = document.getElementById('screenoverlay');
+    overlayElement.classList.add('full-screen-overlay');
+    overlayElement.classList.remove('hidden_class');
+    let popupContent = document.createElement('div');
+    popupContent.classList.add('popup_window');
+    let acindex = index;
+    popupContent.innerHTML = `
+        <div class='infobox'>
+        <div class='close'>
+        <button onclick='InfoContactClose()'><img src='./images/icons/x-mark.png'><button>
+        </div>
+        <div class='information'>
+        <form>
+            <label>Name:</label><br>
+            <input type="text" id="name" placeholder="name" value='${accounts[index].name}'></input><br>
+            <label>Last Name:</label><br>
+            <input type="text" id="lastname" placeholder="last name" value='${accounts[index].lastname}'></input><br>
+            <label>Phone:</label><br>
+            <input type="number" id="phone" placeholder="phone" value='${accounts[index].phone}'></input><br>
+            <label>Email:</label><br>
+            <input type="text" id="email" placeholder="email" value='${accounts[index].email}'></input><br>
+            <label>City:</label><br>
+            <input type="text" id="city" placeholder="city" value='${accounts[index].city}'></input><br>
+            <label>Address:</label><br>
+            <input type="text" id="address" placeholder="address" value='${accounts[index].address}'></input><br>
+            <label>Image URL:<br> 
+            <input type="text" id="image" placeholder="URL of image to use as avatar" value='${accounts[index].profilepiclink}'></input><br>
+        </form>
+        <br>
+        <button class="sendinfobutton" onclick="sendInfoEdit('${index}')">Change</button>
         </div>
         </div>
     `;
@@ -225,10 +266,9 @@ function AddContact() {
 function CheckExisting(name,lastname,phone)
 {
     let code = 3;
-    alert("Checkingexisting");
     accounts.forEach((elem, index) => 
     {
-        if(elem.name == name && elem.lastname == lastname)
+        if(elem.name.toLowerCase() == name.toLowerCase() && elem.lastname.toLowerCase() == lastname.toLowerCase())
             code = 1;
     });
 
@@ -242,7 +282,114 @@ function CheckExisting(name,lastname,phone)
     }
     return code;
 
+}
+
+function CheckExistingWithEdit(name,lastname,phone,indexofprofile)
+{
+    let code = 3;
+    accounts.forEach((elem, index) => 
+    {
+        if(elem.name.toLowerCase() == name.toLowerCase() && elem.lastname.toLowerCase() == lastname.toLowerCase() && indexofprofile!= index)
+            code = 1;
+    });
+
+    if(code == 3)
+    {
+    accounts.forEach((elem, index ) => 
+        {
+            if(elem.phone == phone && indexofprofile!= index)
+                code = 2;
+        });
+    }
+
+    return code;
+
+}
+
+function sendInfoEdit(accindex)
+{
+    event.preventDefault();
+
+    // Use `let` instead of `const` to allow reassignment
+    let contact_name = '';
+    let contact_lastname = '';
+    let contact_phone = '';
+    let contact_email = '';
+    let contact_city = '';
+    let contact_address = '';
+    let contact_image = '';
+
     
+
+    const container = document.getElementsByClassName("contact_container")[0];
+
+    contact_name = document.getElementById('name').value;
+    contact_lastname = document.getElementById('lastname').value;
+    contact_phone = document.getElementById('phone').value;
+    contact_email = document.getElementById('email').value;
+    contact_city = document.getElementById('city').value;
+    contact_address = document.getElementById('address').value;
+    contact_image = document.getElementById('image').value;
+
+
+    let infoarr = [contact_name, contact_lastname, contact_phone, contact_email, contact_city, contact_address];
+    let flag = false;
+
+    infoarr.forEach(elem =>
+    {
+        if(elem == '')
+            flag = true;
+    });
+
+    if(flag)
+    {
+        alert("Please fill all empty spots!");
+    }
+    else
+    {
+        if(contact_image== null || contact_image == '')
+            contact_image = './images/ProfilePicPlaceholder.jpg';
+
+        const code = CheckExistingWithEdit(contact_name, contact_lastname, contact_phone,accindex);
+        const validation = validator(contact_phone, contact_email);
+
+        if(!flag)
+        {
+            if (validation == 0)
+            {
+                if (code == 3)
+                {
+                    accounts[accindex] =
+                    {
+                        name: contact_name,
+                        lastname: contact_lastname,
+                        phone: contact_phone,
+                        email: contact_email,
+                        city: contact_city,
+                        address: contact_address,
+                        profilepiclink: contact_image
+                    };
+
+                    container.innerHTML = "";
+
+                    let overlayElement = document.getElementById('screenoverlay');
+                    overlayElement.innerHTML = '';
+
+                    InfoContactClose();
+                    create();
+
+                }
+                if (code == 1)
+                {
+                    alert("Name already used!");
+                }
+                if(code == 2)
+                {
+                    alert("Number already used!");
+                }
+            }
+        }
+    }
 }
 
 function sendInfo()
@@ -262,8 +409,6 @@ function sendInfo()
 
     const container = document.getElementsByClassName("contact_container")[0];
 
-    alert("Sent");
-    // Assign values from input fields
     contact_name = document.getElementById('name').value;
     contact_lastname = document.getElementById('lastname').value;
     contact_phone = document.getElementById('phone').value;
@@ -274,9 +419,6 @@ function sendInfo()
 
 
     let infoarr = [contact_name, contact_lastname, contact_phone, contact_email, contact_city, contact_address];
-
-    alert(infoarr);
-
     let flag = false;
 
     infoarr.forEach(elem =>
@@ -291,6 +433,9 @@ function sendInfo()
     }
     else
     {
+        if(contact_image== null || contact_image == '')
+            contact_image = './images/ProfilePicPlaceholder.jpg';
+
         const code = CheckExisting(contact_name, contact_lastname, contact_phone);
         const validation = validator(contact_phone, contact_email);
 
@@ -360,6 +505,9 @@ function validator(contact_phone,contact_email)
 
 
 
+
+
+
 function RemoveContact (index)
 
 {
@@ -371,3 +519,34 @@ function RemoveContact (index)
     create();
     }
 }
+
+
+
+
+
+function Party() {
+
+    alert("Party time!");
+
+    const images = [
+        'url(./images/Palm_Tree_BG_Party1.png)',
+        'url(./images/Palm_Tree_BG_Party2.png)',
+        'url(./images/Palm_Tree_BG_Party3.png)',
+        'url(./images/Palm_Tree_BG_Party4.png)',
+        'url(./images/Palm_Tree_BG_Party5.png)',
+        'url(./images/Palm_Tree_BG_Main.png)'
+    ];
+
+   
+    let index = 0;
+    const interval = setInterval(() => {
+        document.body.style.backgroundImage = images[index];
+        index++;
+
+        if (index === images.length) {
+            index = 0;
+        }
+    }, 500); 
+    
+}
+
